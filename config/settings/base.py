9a7424514a06a646c7b416163f683b7b22413d8c
khayratdinov <khayratdinov.np@gmail.com>
2025-01-19 21:44:36 +0500

    serve django NGINX and customize logging

--- config/settings/base.py
+++ config/settings/base.py
@@ -60,6 +60,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "apps.user_auth.middleware.CustomHeaderMiddleware",
 ]
 
 ROOT_URLCONF = "config.urls"
