11c8c11887b146f6642149cee72cc21f6e263302
khayratdinov <khayratdinov.np@gmail.com>
2025-01-30 23:05:37 +0500

    Remove common app

--- apps/common/api/v1/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import SampleAPIView
-
-urlpatterns = [
-    path('sample/', SampleAPIView.as_view(), name='sample-api'),
-]
--- apps/common/api/v1/views.py
+++
@@ -1,6 +0,0 @@
-from rest_framework.views import APIView
-from rest_framework.response import Response
-
-class SampleAPIView(APIView):
-    def get(self, request, *args, **kwargs):
-        return Response({'message': 'Hello from API v1!'})
